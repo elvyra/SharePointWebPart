b87ae5dea1874081fbe5b227e1fd950708b8c9b2
Elvyra Sabaliauskiene <elvyra.sabaliauskiene@gmail.com>
2020-07-02 13:58:11 +0300

    init

---
+++ src/webparts/saskaitos/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISaskaitosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SaskaitosWebPartStrings' {
+  const strings: ISaskaitosWebPartStrings;
+  export = strings;
+}
